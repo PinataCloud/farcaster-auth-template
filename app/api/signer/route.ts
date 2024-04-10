36f68d06bd00bbde22c0a86c734bd1e3efd11198
Steve <stevedsimkins@gmail.com>
2024-04-09 20:37:13 -0400

    init

---
+++ app/api/signer/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse, NextRequest } from "next/server";
+import { PinataFDK } from "pinata-fdk";
+
+const fdk = new PinataFDK({
+  pinata_jwt: process.env.PINATA_JWT as string,
+  pinata_gateway: "",
+  appFid: process.env.APP_FID as string,
+  appMnemonic: process.env.DEVELOPER_MNEMONIC,
+});
+
+export async function POST() {
+  try {
+    const res = await fdk.createSponsoredSigner();
+    return NextResponse.json(res);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(error);
+  }
+}
+
+export async function GET(request: NextRequest) {
+  try {
+    const searchParams = request.nextUrl.searchParams;
+    const token: any = searchParams.get("token");
+    const res = await fdk.pollSigner(token);
+    console.log(res)
+    return NextResponse.json(res);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(error);
+  }
+}
